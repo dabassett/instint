79b178008bba02e8e22bd6fe5cd2cc66986c64be
Alex Bassett <alex.bassett@trellix.com>
2023-12-21 16:48:51 -0500

    basic swatches added with some experimental reactivity features

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+
+
+const randomColor = () => {
+  let hex_color = Math
+    .floor(Math.random()*16777215)
+    .toString(16)
+    .padStart(6, '0');
+  return `#${hex_color}`;
+}
+
+let initialSwatches = [
+  {id: 0, color: randomColor(), parent: null},
+]
+
+let nextId = initialSwatches.length;
+
+export default function App() {
+  const [lastColor, setLastColor] = useState("#000000");
+  const [swatches, setSwatches] = useState(initialSwatches);
+
+
+  const handleSwatchClick = (id) => {
+    let newColor = randomColor()
+    setLastColor(newColor)
+
+    // TODO: swatches should be an object
+    //       also causing parent/child updating to happen out of order
+    const nextSwatches = swatches.map(swatch => {
+      if (swatch.id === id) {
+        return {
+          ...swatch,
+          color: newColor
+        };
+      } else if (swatch.parent?.id === id) {
+        return {
+          ...swatch,
+          parent: swatches[id]
+        };
+      }
+      return swatch;
+    });
+    setSwatches(() => nextSwatches);
+  }
+
+
+  // todo separate text tags from swatch components
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          Here we go I guess
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Instint</h1>
+      <h2
+      style={{
+        color: lastColor,
+      }}>Generate an entire color palette from a single input!</h2>
+
+      <button onClick={() => {
+        setSwatches([
+          ...swatches,
+          { id: nextId++, color: randomColor(), parent: swatches[0] }
+        ]);
+      }}>Add Swatch</button>
+
+      {swatches.map(swatch => (
+        <Swatch key={swatch.id} id={swatch.id} color={swatch.color} parent={swatch.parent} onClick={handleSwatchClick} />
+      ))}
     </div>
   );
 }
 
-export default App;
+
+function Swatch({ id, color, parent, onClick }) {
+  return (
+    <div
+      style={{
+        width: '50px',
+        height: '50px',
+        backgroundColor: parent?.color || color,
+        cursor: 'pointer',
+      }}
+      onClick={() => onClick(id)}
+    >
+    </div>
+  );
+}
+
+
+//function AddSwatchButton({ onClick })

92ca720f4bdd57aad9b9cf237ae912e7744c475c
Alex Bassett <alex.bassett@trellix.com>
2023-12-30 13:39:11 -0500

    style(core): add eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-
-
+import { useState } from "react";
+import tc from "tinycolor";
 
 const randomColor = () => {
-  let hex_color = Math
-    .floor(Math.random()*16777215)
-    .toString(16)
-    .padStart(6, '0');
-  return `#${hex_color}`;
+  // let hex_color = Math
+  //   .floor(Math.random()*16777215)
+  //   .toString(16)
+  //   .padStart(6, '0');
+  // return `#${hex_color}`;
+  return tc.random().toHexString();
 };
 
-let initialSwatches = {
-  0: {color: randomColor(), parentId: null},
+const initialSwatches = {
+  0: { color: randomColor(), parentId: null },
 };
 
 let nextId = Object.keys(initialSwatches).length;
@@ -19,7 +19,6 @@ let nextId = Object.keys(initialSwatches).length;
 export default function App() {
   const [lastColor, setLastColor] = useState("#000000");
   const [swatches, setSwatches] = useState(initialSwatches);
-
 
   const handleSwatchClick = (id) => {
     let newColor = randomColor();
@@ -31,52 +30,66 @@ export default function App() {
         ...swatches[id],
         color: newColor,
       },
-    }
+    };
 
     setSwatches(() => nextSwatches);
   };
-
 
   // todo separate text tags from swatch components
   return (
     <div className="App">
       <h1>Instint</h1>
       <h2
-      style={{
-        color: lastColor,
-      }}>Generate an entire color palette from a single input!</h2>
+        style={{
+          color: lastColor,
+        }}
+      >
+        Generate an entire color palette from a single input!
+      </h2>
 
-      <button onClick={() => {
-        setSwatches({
-          ...swatches,
-          // todo: hardcoded parentId
-          [nextId++]: { color: "#00ff00", parentId: 0 }
-        });
-      }}>Add Swatch</button>
+      <button
+        onClick={() => {
+          setSwatches({
+            ...swatches,
+            // todo: hardcoded parentId
+            [nextId++]: { color: "#00ff00", parentId: 0 },
+          });
+        }}
+      >
+        Add Swatch
+      </button>
 
-      {Object.keys(swatches).map(id => (
-        <Swatch key={id} id={id} swatches={swatches} onClick={handleSwatchClick} />
+      {Object.keys(swatches).map((id) => (
+        <Swatch
+          key={id}
+          id={id}
+          swatches={swatches}
+          onClick={handleSwatchClick}
+        />
       ))}
     </div>
   );
 }
 
-
 function Swatch({ id, swatches, onClick }) {
-  const swatch = swatches[id]
+
+  const swatch = swatches[id];
+
+  const computeColor = () => {
+    return swatches[swatch.parentId]?.color || swatch.color;
+  };
+
   return (
     <div
       style={{
-        width: '50px',
-        height: '50px',
-        backgroundColor: swatches[swatch.parentId]?.color || swatch.color,
-        cursor: 'pointer',
+        width: "50px",
+        height: "50px",
+        backgroundColor: computeColor(),
+        cursor: "pointer",
       }}
       onClick={() => onClick(id)}
-    >
-    </div>
+    ></div>
   );
 }
-
 
 //function AddSwatchButton({ onClick })
--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-
-const container = document.getElementById('root');
-
-const root = createRoot(container)
+const root = createRoot(document.getElementById("root"));
 
 root.render(
-  <App name="Instint" />
+  <StrictMode>
+    <App name="Instint" />
+  </StrictMode>,
 );

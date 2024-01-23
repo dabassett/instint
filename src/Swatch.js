3b5dbac5de9507c96862ed995837e6ccaec523ce
Alex Bassett <alex.bassett@trellix.com>
2024-01-23 18:08:41 -0500

    style(Swatch): add reactive transitions to swatches

--- src/Swatch.js
+++ src/Swatch.js
@@ -1,16 +1,31 @@
-import { derive, toHex } from "./utils.js";
+import { styled } from '@mui/material/styles';
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
+
+import { derive, toHex } from "./utils.js";
+
+const ReactiveCard = styled(Card)`
+  ${({ theme }) => `
+  cursor: pointer;
+  transition: ${theme.transitions.create(['background-color', 'transform'], {
+    duration: theme.transitions.duration.shorter,
+  })};
+  &:hover {
+    transform: scale(1.05);
+  }
+  `}
+`;
 
 export default function Swatch({ id, hswl, onClick }) {
   const color = toHex(hswl);
   const textColor = toHex(
     derive(hswl, { contrast: 5, adjustHue: 15, adjustSat: -0.2 }),
   );
+
   return (
-    <Card
+    <ReactiveCard
       variant="outlined"
       sx={{ width: 120, height: 60 }}
       style={{
@@ -20,7 +35,8 @@ export default function Swatch({ id, hswl, onClick }) {
     >
       <CardContent>
         <Typography
-          sx={{ fontSize: 22 }}
+          variant="h6"
+          //sx={{ fontSize: 22 }}
           style={{
             color: textColor,
           }}
@@ -29,6 +45,6 @@ export default function Swatch({ id, hswl, onClick }) {
           {color}
         </Typography>
       </CardContent>
-    </Card>
+    </ReactiveCard>
   );
 }

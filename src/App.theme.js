7c34d2614e4999fc1c43e12beabc7ef5879103d6
Alex Bassett <alex.bassett@trellix.com>
2024-01-23 18:07:40 -0500

    style: expand the main theme with custom typography and fonts

--- src/App.theme.js
+++ src/App.theme.js
@@ -1,10 +1,112 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+  typography: {
+    fontFamily: [
+      "Raleway",
+      '"Helvetica Neue"',
+      "Arial",
+      "sans-serif"
+    ].join(",",),
+    fontWeightLight: 100,
+    fontWeightRegular: 300,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+    h1: {
+      fontWeight: 100,
+    },
+    h2: {
+      fontWeight: 100,
+    },
+    h3: {
+      fontWeight: 100,
+    },
+    h4: {
+      fontWeight: 100,
+    },
+    h5: {
+      fontWeight: 300,
+    },
+    h6: {
+      fontWeight: 300,
+    },
+    body1: {
+      fontWeight: 500,
+    },
+  },
+  props: {
+    MuiButton: {
+      size: "large",
+    },
+    MuiButtonGroup: {
+      size: "small",
+    },
+    MuiCheckbox: {
+      size: "small",
+    },
+    MuiFab: {
+      size: "small",
+    },
+    MuiFormControl: {
+      margin: "dense",
+      size: "small",
+    },
+    MuiFormHelperText: {
+      margin: "dense",
+    },
+    MuiIconButton: {
+      size: "small",
+    },
+    MuiInputBase: {
+      margin: "dense",
+    },
+    MuiInputLabel: {
+      margin: "dense",
+    },
+    MuiRadio: {
+      size: "small",
+    },
+    MuiSwitch: {
+      size: "small",
+    },
+    MuiTextField: {
+      margin: "dense",
+      size: "small",
+    },
+  },
   components: {
+
     MuiStack: {
       defaultProps: {
         useFlexGap: true,
+      },
+    },
+    MuiSlider: {
+      styleOverrides: {
+        root: {}
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          borderRadius: 2,
+        }
+      },
+    },
+    MuiCard: {
+      styleOverrides: {
+        root: {
+          borderRadius: 2,
+        }
       },
     },
   },

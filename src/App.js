4d1fa5f5b85afccd35d96e899ee83a685a242a64
Alex Bassett <alex.bassett@trellix.com>
2024-02-01 15:20:34 -0500

    feat(App): add usage instructions

--- src/App.js
+++ src/App.js
@@ -376,35 +376,35 @@ export default function App() {
             >
               <PaletteTab label="Welcome" value="0" {...paletteTabProps} />
               <PaletteTab label="How To" value="1" {...paletteTabProps} />
+              <PaletteTab label="About" value="2" {...paletteTabProps} />
             </TabList>
             <TabPanel value="0" sx={{ my: 3, p: 0 }}>
               <Fade in>
                 <Card {...cardProps}>
                   <CardContent>
                     <Typography
-                      variant="h2"
+                      variant="h3"
                       style={{ color: palette1.textA }}
                       gutterBottom
                     >
-                      Welcome to Instint!
+                      Welcome!
                     </Typography>
                     <Typography
                       variant="h5"
                       style={{ color: palette1.textAA }}
                       gutterBottom
                     >
-                      Here to help designers pair text and background colors
-                      that are both beautiful and easy to read
+                      All of the colors you see were derived automatically from
+                      the background color in this box.
                     </Typography>
                     <Typography
                       variant="body1"
                       style={{ color: palette1.textAAA }}
                       gutterBottom
                     >
-                      Instint can take any color and generate analogous colors
-                      that satisfy WCAG 2.1 contrast ratio requirements. This
-                      means that you no longer need to fiddle with finicky
-                      formulae to create perfect color palettes.
+                      Try it out! Click the refresh button above to roll a whole
+                      new look, or use the color controls at the bottom to
+                      fine-tune your own design.
                     </Typography>
                   </CardContent>
                 </Card>
@@ -415,11 +415,137 @@ export default function App() {
                 <Card {...cardProps}>
                   <CardContent>
                     <Typography
-                      variant="h2"
+                      variant="h3"
                       style={{ color: palette1.textA }}
                       gutterBottom
                     >
                       How To Use Instint:
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ color: palette1.textAAA }}
+                      gutterBottom
+                    >
+                      For best results turn off your blue light filter and any
+                      browser extensions that might interfere with
+                      Instint&apos;s colors.
+                    </Typography>
+                    <Typography
+                      variant="h5"
+                      style={{ color: palette1.textAA }}
+                      gutterBottom
+                      sx={{ mt: 3 }}
+                    >
+                      Resetting
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ color: palette1.textAAA }}
+                      gutterBottom
+                    >
+                      Clicking the refresh button at the top of the page will
+                      erase any changes and build a new palette. Use it when you
+                      need to start fresh.
+                    </Typography>
+                    <Typography
+                      variant="h5"
+                      style={{ color: palette1.textAA }}
+                      gutterBottom
+                      sx={{ mt: 3 }}
+                    >
+                      Editing Swatches
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ color: palette1.textAAA }}
+                      gutterBottom
+                    >
+                      Click a swatch from the list to select it and use the
+                      sliders at the bottom to edit its hue, saturation, and
+                      luminance attributes.
+                    </Typography>
+                    <Typography
+                      variant="h5"
+                      style={{ color: palette1.textAA }}
+                      gutterBottom
+                      sx={{ mt: 3 }}
+                    >
+                      Swatch Inheritance
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ color: palette1.textAAA }}
+                      gutterBottom
+                    >
+                      Most swatches are inheriting their attributes from the
+                      first one in the palette. Editing a parent swatch will
+                      automatically update all of its children.
+                    </Typography>
+                    <Typography
+                      variant="h5"
+                      style={{ color: palette1.textAA }}
+                      gutterBottom
+                      sx={{ mt: 3 }}
+                    >
+                      Inheritance Modes
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ color: palette1.textAAA }}
+                      gutterBottom
+                    >
+                      Child swatches have additional settings that appear next
+                      to their attributes. These control how parent and child
+                      swatches&apos; attributes are combined.
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      style={{ color: palette1.textAA }}
+                      gutterBottom
+                      sx={{ mt: 1 }}
+                    >
+                      Adjust (ADJ)
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ color: palette1.textAAA }}
+                      gutterBottom
+                    >
+                      The child&apos;s value is added to the parent&apos;s.
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      style={{ color: palette1.textAA }}
+                      gutterBottom
+                      sx={{ mt: 1 }}
+                    >
+                      Fixed (FIX)
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ color: palette1.textAAA }}
+                      gutterBottom
+                    >
+                      Only the child&apos;s value will be used. Use fixed when
+                      you want a child swatch to ignore changes to the parent
+                      for one or more attributes.
+                    </Typography>
+                    <Typography
+                      variant="h6"
+                      style={{ color: palette1.textAA }}
+                      gutterBottom
+                      sx={{ mt: 1 }}
+                    >
+                      Contrast
+                    </Typography>
+                    <Typography
+                      variant="body1"
+                      style={{ color: palette1.textAAA }}
+                      gutterBottom
+                    >
+                      This setting controls the relative contrast ratio between
+                      the parent and child colors. Higher values are more
+                      suitable for smaller text. (Only applies to luminance)
                     </Typography>
                   </CardContent>
                 </Card>
